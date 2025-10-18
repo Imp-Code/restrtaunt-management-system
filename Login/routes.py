97c8e7dd7b530e343776352b2c2ff8869788dfc9
Imp-Code <96018199+Imp-Code@users.noreply.github.com>
2025-10-18 11:32:38 -0400

    Update routes.py

--- Login/routes.py
+++ Login/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, session, request
 
 login_bp = Blueprint("login",__name__)
 
-@login_bp.route("/login")
+@login_bp.route("/")
 def login():
     return render_template("login.html")
 
